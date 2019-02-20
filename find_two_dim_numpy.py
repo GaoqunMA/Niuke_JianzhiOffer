0552d7e0a0bced0f0543c11d356d2cd67c71159e
GaoqunMa <17121711@bjtu.edu.cn>
2019-02-20 16:15:21 +1100

    test

---
+++ find_two_dim_numpy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@time: 2019/2/20 16:04
+@author: Gaoqun Ma
+@file: find_two_dim_numpy.py
+"""
+"""
+在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+"""
+class Solution:
+    def Find(self, target, array):
