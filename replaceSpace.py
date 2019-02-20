2d1ba49ffd505f2f8db7c9c7b4bafc3a5f0abcb6
GaoqunMa <17121711@bjtu.edu.cn>
2019-02-20 17:18:23 +1100

    用指定字符替换字符串中的空格

---
+++ replaceSpace.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@time: 2019/2/20 16:59
+@author: Gaoqun Ma
+@file: replaceSpace.py
+"""
+"""
+请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+"""
+class Solution():
+    def replaceSpace(self, s):
+        if not s:
+            return s
+        else:
+            replace_s = []
+            s = list(s)
+            for alpha in s:
+                if alpha == ' ':
+                    replace_s.append('%20')
+                else:
+                    replace_s.append(alpha)
+            return ''.join(replace_s)
+
+if __name__ == '__main__':
+    gaoqun = Solution()
+    s = gaoqun.replaceSpace('Ma Gao Qun')
+    print s
+
+"""
+知识点：
+1.str可以直接通过list()转化为list，也可以通过.split('')分割字符串变成数组
+2.''.join(list)，可以将list中所有的字符串以指定形式拼接(非常重要，对于字符串去除数字等等有很好的应用)
+方法：
+将字符串先转化为数组，然后做一个判断，在将数组中的字符拼接为字符串。
+时间复杂度为(N)N为字符串的长度，空间复杂度为（N）
+"""
