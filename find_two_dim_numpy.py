68a0ad5035740cbe56c77bda41f605c5d1a864ad
GaoqunMa <17121711@bjtu.edu.cn>
2019-02-20 16:58:36 +1100

    二维有序数组做查找

--- find_two_dim_numpy.py
+++ find_two_dim_numpy.py
@@ -11,3 +11,30 @@
 """
 class Solution:
     def Find(self, target, array):
+        if not array:
+            return False
+
+        rows = len(array)
+        cols = len(array[0])
+        row, col = rows - 1, 0
+        while row >= 0 and col <= cols - 1:
+            if array[row][col] == target:
+                return True
+            elif array[row][col] < target:
+                col = col + 1
+            else:
+                row = row - 1
+        return False
+
+"""
+知识点：
+1.明确数据的索引的时间复杂度为1
+2.数组只有len的方法，numpy才有shape，size和len的方法，所以想知道数组的维度只能len
+3.一个数组若为空，直接判断即可，为空输出为false
+方法一：
+直接遍历二维数组，时间复杂度为（N*M）
+方法二：
+从左下角或者右上角一步一步的搜索，举例来说，从左下角开始，如果target大于本值，向左移动；若小于本值，则向上移动。这也是不从左上角开始的原因
+如果大于，左上角可以有两个移动方向，移动的方向就是不确定的了
+"""
+
